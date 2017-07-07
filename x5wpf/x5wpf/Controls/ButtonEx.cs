f54ecfc8a9936effd2f477fb002c97fb51b02ff3
Zorin Ivan <zorin.i@en-gy.ru>
2017-07-07 20:32:16 +0300

    Кнопочки

--- x5wpf/x5wpf/Controls/ButtonEx.cs
+++ x5wpf/x5wpf/Controls/ButtonEx.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,33 +18,6 @@ using System.Windows.Shapes;
 namespace x5wpf.Controls
 {
     /// <summary>
-    /// Follow steps 1a or 1b and then 2 to use this custom control in a XAML file.
-    ///
-    /// Step 1a) Using this custom control in a XAML file that exists in the current project.
-    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
-    /// to be used:
-    ///
-    ///     xmlns:MyNamespace="clr-namespace:x5wpf.Controls"
-    ///
-    ///
-    /// Step 1b) Using this custom control in a XAML file that exists in a different project.
-    /// Add this XmlNamespace attribute to the root element of the markup file where it is 
-    /// to be used:
-    ///
-    ///     xmlns:MyNamespace="clr-namespace:x5wpf.Controls;assembly=x5wpf.Controls"
-    ///
-    /// You will also need to add a project reference from the project where the XAML file lives
-    /// to this project and Rebuild to avoid compilation errors:
-    ///
-    ///     Right click on the target project in the Solution Explorer and
-    ///     "Add Reference"->"Projects"->[Browse to and select this project]
-    ///
-    ///
-    /// Step 2)
-    /// Go ahead and use your control in the XAML file.
-    ///
-    ///     <MyNamespace:ButtonEx/>
-    ///
     /// </summary>
     public class ButtonEx : Button, INotifyPropertyChanged
     {
@@ -51,59 +25,62 @@ namespace x5wpf.Controls
         /// <summary>
         /// Ширина прямоугольника кнопки
         /// </summary>
-        public static readonly DependencyProperty RectangleWidthProperty = DependencyProperty.Register("RectangleWidth", typeof(GridLength), typeof(ButtonEx),
-            new PropertyMetadata(GridLength.Auto, new PropertyChangedCallback((d, e) =>
+        public static readonly DependencyProperty RectangleWidthProperty = DependencyProperty.Register("RectangleWidth", typeof(Double), typeof(ButtonEx),
+            new PropertyMetadata(Double.NaN, new PropertyChangedCallback((d, e) =>
             {
                 var x = d as ButtonEx;
                 if (x != null)
                 {
-                    x.RectangleWidth = (GridLength)e.NewValue;
+                    x.RectangleWidth = (Double)e.NewValue;
                 }
                 StaticPropertyChanged(d, "RectangleWidth");
             })));
-        public GridLength RectangleWidth
+        public Double RectangleWidth
         {
-            get { return (GridLength)GetValue(RectangleWidthProperty); }
+            get { return (Double)GetValue(RectangleWidthProperty); }
             private set { SetValue(RectangleWidthProperty, value); }
         }
 
-        public static readonly DependencyProperty StartPointProperty = DependencyProperty.Register("StartPoint", typeof(Point), typeof(ButtonEx),
-            new PropertyMetadata(new Point(0, 0), new PropertyChangedCallback((d, e) => { StaticPropertyChanged(d, "StartPoint"); })));
-        public Point StartPoint
+        /// <summary>
+        /// Ширина носика кнопки
+        /// </summary>
+        public static readonly DependencyProperty NoseWidthProperty = DependencyProperty.Register("NoseWidth", typeof(Double), typeof(ButtonEx),
+            new PropertyMetadata(Double.NaN, new PropertyChangedCallback((d, e) =>
+            {
+                var x = d as ButtonEx;
+                if (x != null)
+                {
+                    x.NoseWidth = (Double)e.NewValue;
+                    x.RectangleWidth = x.Width - x.NoseWidth;
+
+                }
+                StaticPropertyChanged(d, "NoseWidth");
+            })));
+        public Double NoseWidth
         {
-            get { return (Point)GetValue(StartPointProperty); }
-            private set { SetValue(StartPointProperty, value); }
+            get { return (Double)GetValue(NoseWidthProperty); }
+            set { SetValue(NoseWidthProperty, value); }
         }
 
-        public static readonly DependencyProperty EndPointProperty = DependencyProperty.Register("EndPoint", typeof(Point), typeof(ButtonEx),
-            new PropertyMetadata(new Point(0, 0), new PropertyChangedCallback((d, e) => { StaticPropertyChanged(d, "EndPoint"); })));
-        public Point EndPoint
-        {
-            get { return (Point)GetValue(EndPointProperty); }
-            private set { SetValue(EndPointProperty, value); }
-        }
+        public Point StartPoint { get; private set; }
 
-        public static readonly DependencyProperty Point1Property = DependencyProperty.Register("Point1", typeof(Point), typeof(ButtonEx),
-            new PropertyMetadata(new Point(0, 0), new PropertyChangedCallback((d, e) => { StaticPropertyChanged(d, "Point1"); })));
-        public Point Point1
-        {
-            get { return (Point)GetValue(Point1Property); }
-            private set { SetValue(Point1Property, value); }
-        }
+        public Point EndPoint { get; private set; }
 
-        public static readonly DependencyProperty Point2Property = DependencyProperty.Register("Point2", typeof(Point), typeof(ButtonEx),
-            new PropertyMetadata(new Point(0, 0), new PropertyChangedCallback((d, e) => { StaticPropertyChanged(d, "Point2"); })));
-        public Point Point2
-        {
-            get { return (Point)GetValue(Point2Property); }
-            private set { SetValue(Point2Property, value); }
-        }
+        public Point Point1 { get; private set; }
 
+        public Point Point2 { get; private set; }
+
+        /// <summary>
+        /// Направление кнопки - влево или вправо
+        /// </summary>
         public static readonly DependencyProperty DirectionProperty = DependencyProperty.Register("Direction", typeof(ButtonDirection), typeof(ButtonEx),
             new PropertyMetadata(ButtonDirection.Left, new PropertyChangedCallback((d, e) =>
             {
-                StaticPropertyChanged(d, "Direction");
-                StaticPropertyChanged(d, "Corners");
+                var x = d as ButtonEx;
+                if (x != null)
+                {
+                    x.Direction = (ButtonDirection)e.NewValue;
+                }
             })));
         public ButtonDirection Direction
         {
@@ -114,16 +91,19 @@ namespace x5wpf.Controls
                 switch (value)
                 {
                     case ButtonDirection.Right:
-                        Corners = "5,0,0,5";
+                        Corners = new CornerRadius(5, 0, 0, 5);
                         break;
                     case ButtonDirection.Left:
-                        Corners = "0,5,5,0";
+                        Corners = new CornerRadius(0, 5, 5, 0);
                         break;
                 }
+                RenderButton(ArcSize, Width, value);
             }
         }
 
-
+        /// <summary>
+        /// Размер дуги. 
+        /// </summary>
         public static readonly DependencyProperty ArcSizeProperty = DependencyProperty.Register("ArcSize", typeof(Double), typeof(ButtonEx),
                         new PropertyMetadata(Double.NaN, new PropertyChangedCallback((d, e) =>
                         {
@@ -142,103 +122,95 @@ namespace x5wpf.Controls
             get { return (Double)GetValue(ArcSizeProperty); }
             set
             {
-                Point1 = new Point(Point1.X, (Height - ArcSize) / 2);
-                Point2 = new Point(Point2.X, (Height - ArcSize) / 2 + ArcSize);
+                RenderButton(value, Width, Direction);
                 SetValue(ArcSizeProperty, value);
             }
-        }
-
-        /// <summary>
-        /// Позиция "носика" кнопки по горизонтали
-        /// </summary>
-        public static readonly DependencyProperty NosePositionProperty = DependencyProperty.Register("NosePosition", typeof(Int32), typeof(ButtonEx),
-                                    new PropertyMetadata(Int32.MinValue, new PropertyChangedCallback((d, e) =>
-                                    {
-                                        var x = d as ButtonEx;
-                                        if (x != null)
-                                        {
-                                            x.NosePosition = (Int32)e.NewValue;
-                                        }
-
-                                        //StaticPropertyChanged(d, "NosePosition");
-                                        //StaticPropertyChanged(d, "RectangleWidth");
-                                        //StaticPropertyChanged(d, "NoseWidth");
-                                        //StaticPropertyChanged(d, "Point1");
-                                        //StaticPropertyChanged(d, "Point2");
-                                    })));
-        public Int32 NosePosition
-        {
-            get { return (Int32)GetValue(NosePositionProperty); }
-            set
-            {
-                RectangleWidth = new GridLength(value, GridUnitType.Star);
-                NoseWidth = new GridLength(Width - value, GridUnitType.Star);
-                Point1 = new Point(Width - value * 1.2, Point1.Y);
-                Point2 = new Point(Width - value * 1.2, Point2.Y);
-                SetValue(NosePositionProperty, value);
-
-                NotifyPropertyChanged("NosePosition");
-                NotifyPropertyChanged("RectangleWidth");
-                NotifyPropertyChanged("NoseWidth");
-                NotifyPropertyChanged("Point1");
-                NotifyPropertyChanged("Point2");
-            }
-        }
-
-        /// <summary>
-        /// Ширина носика кнопки
-        /// </summary>
-        public static readonly DependencyProperty NoseWidthProperty = DependencyProperty.Register("NoseWidth", typeof(GridLength), typeof(ButtonEx),
-            new PropertyMetadata(GridLength.Auto, new PropertyChangedCallback((d, e) => { StaticPropertyChanged(d, "NoseWidth"); })));
-        public GridLength NoseWidth
-        {
-            get { return (GridLength)GetValue(NoseWidthProperty); }
-            private set { SetValue(NoseWidthProperty, value); }
         }
         #endregion
 
         #region Описание прямоугольника
-
-
         /// <summary>
-        /// Ширина прямоугольника кнопки
+        /// Уголки
         /// </summary>
-        public static readonly DependencyProperty CornersProperty = DependencyProperty.Register("Corners", typeof(String), typeof(ButtonEx),
-            new PropertyMetadata("5,0,0,5", new PropertyChangedCallback((d, e) => { StaticPropertyChanged(d, "Corners"); })));
-        public String Corners
+        public static readonly DependencyProperty CornersProperty = DependencyProperty.Register("Corners", typeof(CornerRadius), typeof(ButtonEx),
+            new PropertyMetadata(new CornerRadius(0), new PropertyChangedCallback((d, e) => { StaticPropertyChanged(d, "Corners"); })));
+        public CornerRadius Corners
         {
-            get { return (String)GetValue(CornersProperty); }
+            get { return (CornerRadius)GetValue(CornersProperty); }
             private set { SetValue(CornersProperty, value); }
         }
-
-
         #endregion
+
+        /// <summary>
+        /// Рисунок носика
+        /// </summary>
+        public static readonly DependencyProperty PathStringProperty = DependencyProperty.Register("PathString", typeof(String), typeof(ButtonEx),
+            new PropertyMetadata(String.Empty, new PropertyChangedCallback((d, e) => { StaticPropertyChanged(d, "PathString"); })));
+        public String PathString
+        {
+            get { return (String)GetValue(PathStringProperty); }
+            set { SetValue(PathStringProperty, value); }
+        }
 
         static ButtonEx()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(ButtonEx), new FrameworkPropertyMetadata(typeof(ButtonEx)));
         }
-
-        public ButtonEx()
-        {
-
-        }
         protected override void OnRenderSizeChanged(SizeChangedInfo sizeInfo)
         {
             if (sizeInfo.HeightChanged)
             {
-                var newArcSize = (sizeInfo.PreviousSize.Height != 0) ? ArcSize * sizeInfo.NewSize.Height / sizeInfo.PreviousSize.Height : ArcSize * sizeInfo.NewSize.Height / 100;
-                ArcSize = newArcSize;
+                ArcSize = (sizeInfo.PreviousSize.Height != 0) ? ArcSize * sizeInfo.NewSize.Height / sizeInfo.PreviousSize.Height : ArcSize * sizeInfo.NewSize.Height / 100;
                 EndPoint = new Point(EndPoint.X, sizeInfo.NewSize.Height);
+
+                NotifyPropertyChanged("StartPoint");
+                NotifyPropertyChanged("EndPoint");
             }
 
             if (sizeInfo.WidthChanged)
             {
-                Point1 = new Point((Width-NosePosition) * 1.1, Point1.Y);
-                Point2 = new Point((Width - NosePosition) * 1.1, Point2.Y);
+                RectangleWidth = sizeInfo.NewSize.Width - NoseWidth;
+
+                NotifyPropertyChanged("RectangleWidth");
             }
 
             base.OnRenderSizeChanged(sizeInfo);
+
+            RenderButton(ArcSize, Width, Direction);
+        }
+
+        /// <summary>
+        /// Отрисовка носика кнопки
+        /// </summary>
+        /// <param name="arcSize">Размер дуги. Отрезок вертикальной прямой, на концах которого должны находиться 2я и 3я точки кубической дуги Безье</param>
+        /// <param name="width">Ширина кнопки</param>
+        /// <param name="direction">Направление носика кнопки</param>
+        private void RenderButton(Double arcSize, Double width, ButtonDirection direction)
+        {
+            if (Direction == ButtonDirection.Left)
+            {
+                StartPoint = new Point(NoseWidth, 0);
+                EndPoint = new Point(NoseWidth, Height);
+                Point1 = new Point(-NoseWidth * 0.1, (Height - arcSize) / 2);
+                Point2 = new Point(-NoseWidth * 0.1, arcSize + (Height - arcSize) / 2);
+                Corners = new CornerRadius(0, 5, 5, 0);
+            }
+            else
+            {
+                StartPoint = new Point(0, 0);
+                EndPoint = new Point(0, Height);
+                Point1 = new Point(NoseWidth * 1.1, (Height - arcSize) / 2);
+                Point2 = new Point(NoseWidth * 1.1, arcSize + (Height - arcSize) / 2);
+                Corners = new CornerRadius(5, 0, 0, 5);
+            }
+            PathString = $"M {StartPoint.X},{StartPoint.Y} C {Point1.X},{Point1.Y} {Point2.X},{Point2.Y} {EndPoint.X},{EndPoint.Y}";
+
+            NotifyPropertyChanged("StartPoint");
+            NotifyPropertyChanged("EndPoint");
+            NotifyPropertyChanged("Point1");
+            NotifyPropertyChanged("Point2");
+            NotifyPropertyChanged("PathString");
+            NotifyPropertyChanged("Corners");
         }
 
         #region NotifyPropertyChanged
@@ -258,8 +230,6 @@ namespace x5wpf.Controls
         }
 
         #endregion
-
-
     }
 
 }
